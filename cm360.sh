e990c75455f18e00912566925e2f9ec683dbde88
cm365 <61925308+cm365@users.noreply.github.com>
2020-03-07 21:39:47 -0500

    Add files via upload

---
+++ cm360.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#CUMMASTERMIND360
+clear
+echo welcome to CUMMASTERMIND365. supervise. track. overcum
+read -p "what is your age " age
+if [ "$age" -le 13 ]
+then 
+	echo wrong answer, you dont even cum
+	exit
+fi
+read -p "how many days has it been since your last cum? " days
+if [ "$days" -le 1 ]
+then
+	echo "EMERGENCY. CUM NOW."
+fi
+clear
+echo YOUR DASHBOARD
+echo days to next cum: 0
+echo "age:"
+echo "$age"
+echo
+echo "user id:"
+head /dev/urandom -c 8
+echo 
+echo "sperm count:"
+echo $RANDOM
+echo "gettin low! eat up bitch"
